4724ea40e8c81eca1953ef3048e6e66adb441664
Amir Malik <a@unoc.net>
2011-08-28 15:24:34 -0700

    Add random sub-100ms delay to response.

--- examples/hello.js
+++ examples/hello.js
@@ -9,7 +9,9 @@ http.createServer(function (req, res) {
     links += '<li><a href="/' + s + '">' + s + '</a></li>';
   });
 
-  res.end('<h1>' + req.url + '</h1>Click any link and watch your RESTalytics dashboard:<ul>' + links + '</ul>');
+  setTimeout(function() {
+    res.end('<h1>' + req.url + '</h1>Click any link and watch your RESTalytics dashboard:<ul>' + links + '</ul>');
+  }, Math.floor(Math.random() * 100));
 }).listen(1337);
 
 console.log('Server running at http://127.0.0.1:1337/');
